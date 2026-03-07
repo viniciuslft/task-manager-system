a272293f9e46844d847fb8dec97f50c2ca9058e5
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-07 18:46:54 -0300

    TMS-59 | :sparkles: Create ProjectFactory

---
+++ backend/database/factories/ProjectFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(3),
+            'description' => fake()->optional()->paragraph(),
+            'status' => fake()->randomElement(['active', 'archived']),
+        ];
+    }
+}
