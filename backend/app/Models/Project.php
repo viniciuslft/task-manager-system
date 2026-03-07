9b687b27ba4f8c9cb26ababb2522400640946632
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-07 14:44:29 -0300

    TMS-31 | :art: Configure Project hasMany Tasks

--- backend/app/Models/Project.php
+++ backend/app/Models/Project.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+    ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
