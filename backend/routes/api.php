3a98b40333afc681a7bcca3243323799b7a54806
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-07 18:38:11 -0300

    TMS-58 | ✨ Configure API rate limiting

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/projects', [ProjectController::class, 'index']);
-Route::post('/projects', [ProjectController::class, 'store']);
-
-Route::get('/projects/{project}/tasks', [TaskController::class, 'index']);
-Route::post('/projects/{project}/tasks', [TaskController::class, 'store']);
-
-Route::patch('/tasks/{task}', [TaskController::class, 'update']);
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    
+    Route::get('/projects/{project}/tasks', [TaskController::class, 'index']);
+    Route::post('/projects/{project}/tasks', [TaskController::class, 'store']);
+    
+    Route::patch('/tasks/{task}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+});
