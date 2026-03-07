b0796c8814db2adf5af886d19afd464e5503ee7d
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-07 15:46:52 -0300

    TMS-39 | ✨ Implement GET /api/projects

--- backend/app/Http/Controllers/Api/ProjectController.php
+++ backend/app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(): AnonymousResourceCollection
+    {
+        $projects = Project::query()
+            ->withCount('tasks')
+            ->latest()
+            ->paginate(10);
+
+        return ProjectResource::collection($projects);
+    }
 }
