c9dd9403845a58a171459649c80d2505aebe7204
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-07 15:04:22 -0300

    TMS-32 | :art: Configure Task belongsTo Project

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
