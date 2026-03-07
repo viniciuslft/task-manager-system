3a9acd592575333d28b256a405f9c8a18011a346
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-07 14:39:36 -0300

    TMS-30 | :art: Add foreign key from tasks to projects

--- backend/database/migrations/2026_03_07_173143_create_tasks_table.php
+++ backend/database/migrations/2026_03_07_173143_create_tasks_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
