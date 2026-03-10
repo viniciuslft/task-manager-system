edb86fe07b2d04699e29898ab9f7ec1b7b219215
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-10 20:56:26 -0300

    TMS-76 | 🎨 Create projects page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ProjectsView from '@/views/ProjectsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsView,
+    },
+  ],
 })
 
 export default router
