cfd4944354de565a6ad9f29d4e10b2f01ab981c6
viniciuslft <viniciuspedrozaestrela@gmail.com>
2026-03-10 22:35:22 -0300

    TMS-90 | 🎨 Create project detail route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProjectsView from '@/views/ProjectsView.vue'
+import ProjectDetailView from '@/views/ProjectDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'projects',
       component: ProjectsView,
+    },
+    {
+      path: '/projects/:id',
+      name: 'project-details',
+      component: ProjectDetailView,
     },
   ],
 })
---
+++ frontend/src/views/ProjectDetailView.vue
@@ -0,0 +1,11 @@
+<template>
+  <AppShell>
+    <div>
+        Project details page
+    </div>
+  </AppShell>
+</template>
+
+<script setup lang="ts">
+import AppShell from '@/components/layout/AppShell.vue'
+</script>
